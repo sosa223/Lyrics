74ae6f81d1ba1fd52f3800a96ef98d0db6f830b7
Chief <75649719+sosa223@users.noreply.github.com>
2025-01-20 14:46:13 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
-  return 0;
-}
+      printf("Now, this is a story all about how\n");
+      printf("My life got flipped-turned upside down\n");
+      printf("And I'd like to take a minute\n");
+      printf("Just sit right there\n");
+      printf("I'll tell you how I became the prince of a town called Bel-Air\n");
+      return 0;
+  }
